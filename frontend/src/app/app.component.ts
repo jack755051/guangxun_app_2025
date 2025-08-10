3c01b5ac68053930553ac6f2f3efcd0570d29971
charlieTai <charlie.tai@tpisoftware.com>
2025-08-10 23:02:24 +0800

    build third version.test

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,22 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Breadcrumb } from "@sanring/gx-breadcrumb";
+// import { GxBreadcrumbComponent } from './../../node_modules/@sanring/gx-breadcrumb/src/lib/gx-breadcrumb.component';
+import { BreadcrumbItem, GxBreadcrumbComponent } from '@sanring/gx-breadcrumb';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Breadcrumb],
+  imports: [RouterOutlet, GxBreadcrumbComponent],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'frontend';
+
+    onBreadcrumbNavigate(item: BreadcrumbItem): void {
+    console.log('導航到:', item);
+    // 這裡可以添加自定義導航邏輯
+    // 例如：記錄分析數據、顯示載入狀態等
+  }
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,36 @@
 import { Routes } from '@angular/router';
-import { GxBreadcrumb } from '@sanring/gx-breadcrumb';
+import { BreadcrumbItem } from '@sanring/gx-breadcrumb';
+
+const breadcrumb: { [key: string]: BreadcrumbItem } = {
+  "home": {
+    label: 'Home',
+    url: '/home'
+  },
+  "about": {
+    label: 'About',
+    url: '/about'
+  },
+  "products": {
+    label: 'Products',
+    url: '/products'
+  },
+  "productDetail": {
+    label: 'Product Detail',
+    url: '/products/:id'
+  }
+};
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
+    data: { breadcrumb: breadcrumb['home'].label }  // 加入這行
   },
   {
     path: 'about',
     loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent),
+    data: { breadcrumb: breadcrumb['about'].label }
   },
   {
     path: 'products',
@@ -17,10 +38,12 @@ export const routes: Routes = [
       {
         path: '',
         loadComponent: () => import('./pages/products/products.component').then(m => m.ProductsComponent),
+        data: { breadcrumb: breadcrumb['products'].label }
       },
       {
         path: ':id',
         loadComponent: () => import('./pages/products/products.component').then(m => m.ProductsComponent),
+        data: { breadcrumb: breadcrumb['productDetail'].label }
       }
     ]
   }
